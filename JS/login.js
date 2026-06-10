885f2a7a52f6a5347e2984d954b28c12db4b1f0b
lanilubyhere <138632076+lanilubyhere@users.noreply.github.com>
2026-06-09 20:04:10 -0600

    Update login.js
    User stories 4-8 JS code

--- JS/login.js
+++ JS/login.js
@@ -1,1 +1,46 @@
+// Sample users
 
+let users = [
+{
+    id: 1,
+    name: "John",
+    email: "owner@test.com",
+    password: "123",
+    role: "owner"
+},
+{
+    id: 2,
+    name: "Sarah",
+    email: "coworker@test.com",
+    password: "123",
+    role: "coworker"
+}
+];
+
+function login() {
+
+    let email =
+        document.getElementById("email").value;
+
+    let password =
+        document.getElementById("password").value;
+
+    let user = users.find(u =>
+        u.email === email &&
+        u.password === password
+    );
+
+    if(user){
+
+        localStorage.setItem(
+            "loggedUser",
+            JSON.stringify(user)
+        );
+
+        window.location.href =
+            "search.html";
+    }
+    else{
+        alert("Invalid Login");
+    }
+}
