9544565bd3e571ca3a042771178ec4df8c14fe0d
UltimateDimentio <ysansultra@gmail.com>
2026-06-17 17:03:19 -0600

    workspace database tables

---
+++ UserSignUpData.js
@@ -0,0 +1,220 @@
+ const person = { 
+        firstName: "Mark", 
+        lastName: "Green", 
+        phone: "1234567890", 
+        email: "DataTest@gmail.com", 
+        password: "PassTest@123",
+        role: "Co-Worker"
+       
+    };
+
+const express = require("express");
+const cors = require("cors"); 
+const sqlite3 = require("sqlite3").verbose();
+const jwt = require('jsonwebtoken');
+ 
+const app = express();
+const PORT = 3001;
+ 
+
+app.use(cors()); 
+app.use(express.json());
+ 
+const db = new sqlite3.Database("./FinalDataBase.db", (err) => {
+    if(err){ console.log(err.message);}
+    else{console.log("Connect to SQLite Database")}
+})
+ 
+db.run(`CREATE TABLE IF NOT EXISTS UserAccount (
+
+	UserID							INTEGER PRIMARY KEY AUTOINCREMENT,
+
+	FirstName						VARCHAR(50)			NOT NULL,
+
+	LastName						VARCHAR(50)			NOT NULL,
+
+	Phone							VARCHAR(50)			NOT NULL,
+
+	Email							VARCHAR(100)		NOT NULL,
+
+    Role                            VARCHAR(100)		NOT NULL,
+
+    Password                        VARCHAR(50)			NOT NULL
+
+)`);
+
+db.run(`CREATE TABLE IF NOT EXISTS Properties (
+
+    PropertyID      INTEGER PRIMARY KEY AUTOINCREMENT,
+	
+    Address         VARCHAR(255)    NOT NULL,
+	
+    Neighborhood    VARCHAR(100)    NOT NULL,
+	
+    Sqft            INTEGER         NOT NULL,
+	
+    Garage          VARCHAR(10)     NOT NULL,
+	
+    Transit         VARCHAR(10)     NOT NULL,
+	
+    CreatedAt       DATETIME        DEFAULT CURRENT_TIMESTAMP
+)`);
+
+db.run(`CREATE TABLE IF NOT EXISTS Workspaces (
+
+    WorkspaceID     INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    PropertyID      INT             NOT NULL,
+	
+    WorkspaceType   VARCHAR(100)    NOT NULL,
+	
+    Seats           INTEGER         NOT NULL,
+	
+    Smoke           VARCHAR(10)     NOT NULL,
+	
+    Date            DATE            NOT NULL,
+	
+    LeaseTerm       INT             NOT NULL,
+
+    LeaseTermUnit   VARCHAR(10)     NOT NULL,
+
+    CreatedAt       DATETIME        DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY     (PropertyID)    REFERENCES Properties (PropertyID)
+)`);
+ 
+app.get("/", (req, res) => {
+    res.send("Data Base Test");
+})
+
+app.get("/Data", (req, res) => {
+ 
+    db.all("SELECT * FROM UserAccount", [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({
+                error: err.message
+            });
+        }
+        res.json(rows);
+    });
+});
+ 
+app.post("/userAccounts", (req, res) => {
+    const {firstName, lastName, phone, email, password, role} = req.body;
+   
+    db.run(`
+        INSERT INTO UserAccount
+        (firstName, lastName, phone, email, password, role)
+        VALUES (?, ?, ?, ?, ?, ?)`, [firstName, lastName, phone, email, password, role], function(err){
+            if(err){
+                return res.status(500).json({error: err.message})
+            }
+       
+        res.json({
+            message: "Person Created",
+            UserID: this.lastID
+        })
+    })
+})
+
+//add propertiys
+
+  app.get("/userAccounts", (req, res) => {
+ 
+    db.all("SELECT * FROM Properties", [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({
+                error: err.message
+            });
+        }
+        res.json(rows);
+    });
+});
+app.post("/Properties", (req, res) => {
+    const { address, neighborhood, sqft, garage, transit } = req.body;
+   
+    db.run(`
+        INSERT INTO Properties
+        (Address, Neighborhood, Sqft, Garage, Transit)
+        VALUES (?, ?, ?, ?, ?)`, 
+        [address, neighborhood, sqft, garage, transit], 
+        function(err) {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+       
+            res.json({
+                message: "Property Added Successfully!",
+                PropertyID: this.lastID
+            });
+        }
+    );
+});
+
+app.delete("/data/:id", (req, res) => {
+    const userId = req.params.id;
+
+    db.run("DELETE FROM Property WHERE PropertyID = ?", [userId], function(err) {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        // this.changes tells us how many rows were actually deleted
+        if (this.changes === 0) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json({ message: `User with ID ${userId} deleted successfully.` });
+    });
+});
+
+//add workspaces
+
+app.get("/workspaces", (req, res) => {
+    db.all("SELECT * FROM Workspaces", [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({
+                error: err.message
+            });
+        }
+        res.json(rows);
+    });
+});
+
+app.post("/workspaces", (req, res) => {
+    const { PropertyID, WorkspaceType, Seats, Smoke, Date, LeaseTerm, LeasTermUnit } = req.body;
+   
+    db.run(`
+        INSERT INTO Workspaces
+        (PropertyID, WorkspaceType, Seats, Smoke, Date, LeaseTerm, LeasTermUnit )
+        VALUES (?, ?, ?, ?, ?, ?, ?)`, 
+        [propertyID, workspaceType, seats, smoke, date, leaseTerm, leasTermUnit ], 
+        function(err) {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+       
+            res.json({
+                message: "Workspace Added Successfully!",
+                Workspaces: this.lastID
+            });
+        }
+    );
+});
+
+app.delete("/workspaces/:id", (req, res) => {
+    const workspaceID = req.params.id;
+
+    db.run("DELETE FROM Workspaces WHERE WorkspaceID = ?", [workspaceID], function(err) {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        // this.changes tells us how many rows were actually deleted
+        if (this.changes === 0) {
+            return res.status(404).json({ message: "Workspace not found" });
+        }
+        res.json({ message: `User with ID ${workspaceID} deleted successfully.` });
+    });
+});
+ 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
