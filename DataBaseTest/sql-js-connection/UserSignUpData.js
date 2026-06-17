b6e4844c7370d6ed9f079c546e197690a5b56cce
tasha524 <tashasian53@gmail.com>
2026-06-17 10:51:26 -0600

    Add files via upload

---
+++ DataBaseTest/sql-js-connection/UserSignUpData.js
@@ -0,0 +1,97 @@
+ const person = { 
+        firstName: "Mark", 
+        lastName: "Green", 
+        phone: "1234567890", 
+        email: "DataTest@gmail.com", 
+        password: "PassTest@123",
+        role: "Co-Worker"
+       
+    };
+
+const express = require("express");
+const cors = require("cors"); 
+const sqlite3 = require("sqlite3").verbose();
+const jwt = require('jsonwebtoken');
+ 
+const app = express();
+const PORT = 3001;
+ 
+
+app.use(cors()); 
+app.use(express.json());
+ 
+const db = new sqlite3.Database("./FinalDataBase.db", (err) => {
+    if(err){ console.log(err.message);}
+    else{console.log("Connect to SQLite Database")}
+})
+ 
+db.run(`CREATE TABLE IF NOT EXISTS UserAccount (
+
+	UserID							INTEGER PRIMARY KEY AUTOINCREMENT,
+
+	FirstName						VARCHAR(50)			NOT NULL,
+
+	LastName						VARCHAR(50)			NOT NULL,
+
+	Phone							VARCHAR(50)			NOT NULL,
+
+	Email							VARCHAR(100)		NOT NULL,
+
+    Role                            VARCHAR(100)		NOT NULL,
+
+    Password                        VARCHAR(50)			NOT NULL
+
+)`)
+ 
+    app.get("/", (req, res) => {
+        res.send("Data Base Test");
+    })
+
+    app.get("/Data", (req, res) => {
+ 
+    db.all("SELECT * FROM UserAccount", [], (err, rows) => {
+        if (err) {
+            return res.status(500).json({
+                error: err.message
+            });
+        }
+        res.json(rows);
+    });
+});
+ 
+app.post("/Data", (req, res) => {
+    const {firstName, lastName, phone, email, password, role} = req.body;
+   
+    db.run(`
+        INSERT INTO UserAccount
+        (firstName, lastName, phone, email, password, role)
+        VALUES (?, ?, ?, ?, ?, ?)`, [firstName, lastName, phone, email, password, role], function(err){
+            if(err){
+                return res.status(500).json({error: err.message})
+            }
+       
+        res.json({
+            message: "Person Created",
+            UserID: this.lastID
+        })
+    })
+})
+
+app.delete("/data/:id", (req, res) => {
+    const userId = req.params.id;
+
+    db.run("DELETE FROM UserAccount WHERE UserID = ?", [userId], function(err) {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        // this.changes tells us how many rows were actually deleted
+        if (this.changes === 0) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json({ message: `User with ID ${userId} deleted successfully.` });
+    });
+});
+ 
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
